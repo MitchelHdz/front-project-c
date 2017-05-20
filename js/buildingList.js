75d192dc570b56215524408ca829bad0922c2e7a
MitchelHdz <sebastianvicky_@hotmail.com>
2017-05-20 12:56:19 -0500

    Update 2017-05-20T17:56:16.256Z

--- js/buildingList.js
+++ js/buildingList.js
@@ -1,29 +1,33 @@
 var iconHeart = document.getElementsByClassName("icon-love");
-
-for (var i = 0; i < iconHeart.length; i++) {
+function iconLovers() {
+  for (var i = 0; i < iconHeart.length; i++) {
     iconHeart[i].addEventListener('click', function( e ){
-	    e = window.event || e; 
-	    if(this === e.target) {
-	        e.preventDefault();
-	        if(this.classList.contains('fa-heart-o')){
-				this.classList.add('fa-heart');
-				this.classList.remove('fa-heart-o');
-			}
-			else if (this.classList.contains('fa-heart')){
-				this.classList.add('fa-heart-o');
-				this.classList.remove('fa-heart');
-			}
-	    }
-	});
+        e = window.event || e; 
+        if(this === e.target) {
+            e.preventDefault();
+            if(this.classList.contains('fa-heart-o')){
+          this.classList.add('fa-heart');
+          this.classList.remove('fa-heart-o');
+        }
+        else if (this.classList.contains('fa-heart')){
+          this.classList.add('fa-heart-o');
+          this.classList.remove('fa-heart');
+        }
+        }
+    });
+  }
 }
+iconLovers();
 var municipality = document.getElementsByClassName("cierralo-municipality");
 function municipalityClick() {
   var toggleMunicipality = function() {
     if(this.classList.contains('active')){
       this.classList.remove('active');
+      buildFilter();
     }
     else{
       this.classList.add('active');
+      buildFilter();
     }
   };
 
@@ -39,9 +43,7 @@ $.ajax({
         dataType: 'json',
         success:function(data) {
           var buildList = JSON.stringify(data);
-          var locationFilter = $('div.cierralo-municipality.cierralo-font-helveltica-neue-light.cursor-pointer');
           for (var i = 0; i < data.Propiedades.length; i++) {
-            console.log(data.Propiedades[i].colonia);
             $('.municipality-container').append('<div class="cierralo-municipality cierralo-font-helveltica-neue-light cursor-pointer">'+ data.Propiedades[i].colonia +'</div>');
             var services = '<div class="cierralo-end-container"><div class="cierralo-all-title cierralo-font-helveltica-medium">'+ data.Propiedades[i].calle +'</div><div class="cierralo-all-subtitle cierralo-font-helveltica-thin">'+ data.Propiedades[i].colonia +'</div><div class="cierralo-all-features cierralo-font-helveltica-medium"><p class="feature-text inline-block first-line">'+ data.Propiedades[i].metrosCuadrados +' m2</p><p class="feature-text inline-block middle-line">'+ data.Propiedades[i].cuartos +' cuartos</p><p class="feature-text inline-block middle-line">'+ data.Propiedades[i].toilets +' baños</p><p class="feature-text inline-block last-line">'+ data.Propiedades[i].PisoEnElQueSeEncuentra +' E</p></div></div>';
             var dudeInfo = '<div class="cierralo-inner-container"><div class="cierralo-all-face cierralo-font-helveltica-thin"><div class="cierralo-face-rounder" style="background: url(\'./img/dude.png\')"></div><p class="owner-name">José</p></div><div class="cierralo-all-fav"><i class="fa fa-heart-o icon-love" aria-hidden="true"></i></div><div class="cierralo-all-price">'+ data.Propiedades[i].precio.substring(1, 4) +' MDP</div>'+ services +'</div>';
@@ -50,10 +52,39 @@ $.ajax({
             sliderRangeValue.push(data.Propiedades[i].precio.substring(1, 4));
           }
           municipalityClick();
+          iconLovers()
+          var seen = {};
+          $('.cierralo-municipality').each(function() {
+              var txt = $(this).text();
+              console.log(txt);
+              if (seen[txt] == true)
+                  $(this).remove();
+              else
+                  seen[txt] = true;
+          });
         }
       });
 sliderRangeValue.sort();
-
+/*function buildFilter() {
+  var filterArray = [];
+  $('.cierralo-building-feature').each(function(index, el) {
+    $(this).show();
+    $(this).removeClass('checked-build');
+  });
+  $('.cierralo-municipality.active').each(function(index, el) {
+      filterArray.push($(this).text());
+      console.log(filterArray);
+  });
+  for(item in filterArray){
+    console.log(item);
+    $('.cierralo-all-subtitle').each(function(index, el) {
+      if($(this).text() == item){
+        $(this).closest('.cierralo-building-feature').addClass('checked-build');
+      }
+    });
+  }
+  $('.cierralo-building-feature').not($('.cierralo-building-feature.checked-build')).hide();
+}*/
 function getVals(){
   // Get slider values
   var parent = this.parentNode;
@@ -66,6 +97,20 @@ function getVals(){
   var slider2 = document.getElementById('slider-2');
       slider1.innerHTML = slide1 + " MDP";
       slider2.innerHTML = slide2 + " MDP";
+  $('.cierralo-all-price').each(function(index, el) {
+    console.log('slider');
+    $(this).closest('cierralo-building-feature').show();
+    var priceValue = $(this).text();
+    priceValue = priceValue.replace("MDP", " ");
+    priceValue = priceValue.replace(",", ".");
+    console.log(priceValue + ' ' + slide1)
+    if(priceValue < slide1 || priceValue > slide2){
+      $(this).closest('.cierralo-building-feature').hide();
+    }
+    else{
+      $(this).closest('.cierralo-building-feature').show();
+    }
+  });
 }
 
 window.onload = function(){
@@ -75,9 +120,6 @@ window.onload = function(){
         var sliders = sliderSections[x].getElementsByTagName("input");
         for( var y = 0; y < sliders.length; y++ ){
           if( sliders[y].type ==="range" ){
-            $('input[type=range]').val(sliderRangeValue[0]);
-            $('input[type=range]').prop('min',sliderRangeValue[0]);
-            $('input[type=range]').prop('max',sliderRangeValue.pop());
             sliders[y].oninput = getVals;
             // Manually trigger event first time to display values
             sliders[y].oninput();

c7ab0d3d5d16ab7b25d112f3e175797bb9f90fd0
MitchelHdz <sebastianvicky_@hotmail.com>
2017-05-22 13:19:48 -0500

    #23 Adding aparment second step script

---
+++ build/js/postApartmentTwo.js
@@ -0,0 +1,27 @@
+// Get the modal
+var alertModal = document.getElementById('alertModal');
+
+// Get the button that opens the modal
+var openModal = document.getElementById("openModal");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var body = document.getElementById('body');
+// When the user clicks on the button, open the modal 
+openModal.onclick = function() {
+	alertModal.classList.add('modal-open');
+	console.log(body);
+	body.classList.add('modal-open');
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	alertModal.classList.remove('modal-open');
+	body.classList.remove('modal-open');
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+		alertModal.classList.remove('modal-open');
+		body.classList.remove('modal-open');
+	}
+}

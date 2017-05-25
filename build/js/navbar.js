4496dabab61ea01f65a0e2fa7be07ba469c7fb95
MitchelHdz <sebastianvicky_@hotmail.com>
2017-05-25 12:14:21 -0500

    #28 Adding navbar hamburguer script

--- build/js/navbar.js
+++ build/js/navbar.js
@@ -1,3 +1,20 @@
+document.querySelector('#cierralo-hamburger-menu').addEventListener('click', function () {
+	this.classList.toggle('is-active');
+	document.querySelector('#cierralo-main-navbar').classList.toggle('cierralo-navbar-style');
+});
+
+document.querySelector('#body').addEventListener('click', function (e) {
+	if (document.querySelector('#cierralo-hamburger-menu').classList.contains('is-active') && e.target.tagName !== 'SPAN' && e.target.tagName !== 'a') {
+		document.querySelector('#cierralo-hamburger-menu').classList.remove('is-active');
+		document.querySelector('#cierralo-main-navbar').classList.remove('cierralo-navbar-style');
+	}
+});
+document.querySelector("#body").addEventListener( "touchstart",function (e) {
+	if (document.querySelector('#cierralo-hamburger-menu').classList.contains('is-active') && e.target.tagName !== 'SPAN' && e.target.tagName !== 'a') {
+		document.querySelector('#cierralo-hamburger-menu').classList.remove('is-active');
+		document.querySelector('#cierralo-main-navbar').classList.remove('cierralo-navbar-style');
+	}
+});
 var dropdown = document.getElementById('dropdown-menu');
 var menu = document.getElementById('userMenu');
 // Get the <span> element that closes the modal

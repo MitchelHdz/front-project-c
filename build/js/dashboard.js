e5d664c57fc60d24867adbee8e8d897b1028f189
MitchelHdz <sebastianvicky_@hotmail.com>
2017-06-02 20:50:30 -0500

    #27 dashboard scripts

--- build/js/dashboard.js
+++ build/js/dashboard.js
@@ -1,9 +1,9 @@
 $('.multiple-items').slick({
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  nextArrow: '<img src="./img/icons/flecha-carrusel.png" class="slider-next cursor-pointer">',
-  prevArrow: '<img src="./img/icons/flecha-carrusel-copy.png" class="slider-prev cursor-pointer">',
+	infinite: true,
+	slidesToShow: 2,
+	slidesToScroll: 2,
+	nextArrow: '<img src="./img/icons/flecha-carrusel.png" class="slider-next cursor-pointer">',
+	prevArrow: '<img src="./img/icons/flecha-carrusel-copy.png" class="slider-prev cursor-pointer">',
 });
 // Get the modal
 var modal = document.getElementById('signModal');
